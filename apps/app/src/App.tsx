934a08cc8fbfe6c218ece3ff4799faae5fc504db
maitysubhasis <hola.maity@gmail.com>
2023-08-01 05:21:57 +0530

    add: fastype

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dev from "./pages/Dev/Dev";
-import Text from "./pages/Dev/Text";
-import Sheet from "./pages/Sheet";
-import { Question } from "./pages/Question";
+import Dev from "./carbon/Dev/Dev";
+import Text from "./carbon/Dev/Text";
+import Sheet from "./carbon/Sheet";
+import { Question } from "./carbon/Question";
+import { FastEditor } from "./fastype/FastEditor";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/text" element={<Text />} />
         <Route path="/sheet" element={<Sheet />} />
         <Route path="/question" element={<Question />} />
+        <Route path="/fastype" element={<FastEditor />} />
         {/* <Route path="/test" element={<Test />} /> */}
       </Routes>
     </Router>
---
+++ apps/app/src/fastype/FastEditor.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Fastype } from "@emrgen/fastype";
+
+export function FastEditor() {
+  return <Fastype />;
+}
