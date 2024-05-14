a84bb3cb36564e8035d56b8c9bc111b35bf41f5d
maitysubhasis <hola.maity@gmail.com>
2024-05-14 20:07:39 +0530

    add: inline style encoding

--- packages/carbon-blocks/src/plugins/InlineStyle.ts
+++ packages/carbon-blocks/src/plugins/InlineStyle.ts
@@ -1,5 +1,13 @@
-import { CarbonPlugin, NodePlugin, NodeSpec } from "@emrgen/carbon-core";
-import { merge } from "lodash";
+import {
+  CarbonPlugin,
+  LinkPath,
+  Node,
+  NodeEncoder,
+  NodePlugin,
+  NodeSpec,
+  TextWriter,
+  Writer,
+} from "@emrgen/carbon-core";
 
 export class InlineStylePlugin extends NodePlugin {
   name = "inlineStyle";
@@ -45,24 +53,59 @@ class InlinePlugin extends NodePlugin {
       },
     };
   }
+
+  encodeInline(
+    w: Writer,
+    ne: NodeEncoder,
+    node: Node,
+    open: string,
+    close: string = open,
+  ) {
+    const writer = new TextWriter();
+    node.children.map((n) => ne.encode(writer, n));
+
+    const text = writer.toString();
+    const prefix = text.match(/^\s+(.*)?/);
+    const suffix = text.match(/(.*)?(\s+)$/);
+
+    if (prefix) {
+      w.write(prefix[2]);
+    }
+
+    w.write(open);
+    // console.log(prefix, suffix, text);
+
+    w.write(text.trim());
+
+    w.write(close);
+    if (suffix) {
+      w.write(suffix[2]);
+    }
+  }
+
+  encode(w: Writer, ne: NodeEncoder, node: Node) {
+    this.encodeInline(w, ne, node, "");
+  }
 }
 
 class CodeSpanPlugin extends InlinePlugin {
   name = "codespan";
 
-  spec(): NodeSpec {
-    return merge(super.spec(), {
-      props: {
-        plugin: {
-          tag: "code",
-        },
-      },
-    });
+  constructor() {
+    super("code");
+  }
+
+  encode(w: Writer, ne: NodeEncoder, node: Node) {
+    this.encodeInline(w, ne, node, "`");
   }
 }
 
 class LinkPlugin extends InlinePlugin {
   name = "link";
+
+  encode(w: Writer, ne: NodeEncoder, node: Node) {
+    this.encodeInline(w, ne, node, "[", `](${node.props.get(LinkPath, "")})`);
+  }
 }
 
 class BoldPlugin extends InlinePlugin {
@@ -71,6 +114,10 @@ class BoldPlugin extends InlinePlugin {
   constructor() {
     super("b");
   }
+
+  encode(w: Writer, ne: NodeEncoder, node: Node) {
+    this.encodeInline(w, ne, node, "**");
+  }
 }
 
 class ItalicPlugin extends InlinePlugin {
@@ -78,6 +125,10 @@ class ItalicPlugin extends InlinePlugin {
 
   constructor() {
     super("em");
+  }
+
+  encode(w: Writer, ne: NodeEncoder, node: Node) {
+    this.encodeInline(w, ne, node, "*");
   }
 }
 
@@ -95,6 +146,10 @@ class UnderlinePlugin extends InlinePlugin {
   constructor() {
     super("u");
   }
+
+  encode(w: Writer, ne: NodeEncoder, node: Node) {
+    this.encodeInline(w, ne, node, "__");
+  }
 }
 
 class StrikethroughPlugin extends InlinePlugin {
@@ -102,6 +157,10 @@ class StrikethroughPlugin extends InlinePlugin {
 
   constructor() {
     super("strike");
+  }
+
+  encode(w: Writer, ne: NodeEncoder, node: Node) {
+    this.encodeInline(w, ne, node, "~");
   }
 }
 
