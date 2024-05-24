5eb9de0421a40eeeb4a3693cb8260788f9f22fd4
maitysubhasis <hola.maity@gmail.com>
2024-05-25 04:33:41 +0530

    fix: carbon-core lint

--- packages/carbon-core/src/core/Block.ts
+++ packages/carbon-core/src/core/Block.ts
@@ -4,42 +4,6 @@ export class Block {
   static isText(node: Node) {
     return node.type.isText;
   }
-
-  // static get(locator: Location | Path | NodeId): Optional<Node> {
-  //
-  //   const {scope, location} = pos;
-  //   const map = StateScope.get(scope);
-  //   if (!map) {
-  //     return null;
-  //   }
-  //
-  //   if (location instanceof NodeId) {
-  //     return map.get(location);
-  //   }
-  //
-  //   if (isArray(location)) {
-  //     return Block.at(location);
-  //   }
-  //
-  //   return null;
-  // }
-  //
-  // static at(path: Path) : Optional<Node> {
-  //   const map = StateScope.get();
-  //   if (!map) {
-  //     return null;
-  //   }
-  //
-  //   let node = map.get(NodeId.ROOT);
-  //   for (let i = 0; i < path.length; i++) {
-  //     if (!node) {
-  //       return null;
-  //     }
-  //     node = node.child(path[i]);
-  //   }
-  //
-  //   return node
-  // }
 }
 
 // @ts-ignore
--- packages/carbon-core/src/utils/log.ts
+++ packages/carbon-core/src/utils/log.ts
@@ -1,22 +1,22 @@
-import { ID } from '../core/ID';
-import { Node } from '../core/Node';
+import { Node } from "../core/Node";
+import { NodeId } from "@emrgen/carbon-core";
 
-export const printId = (id: ID) => {
-	console.log(id.toString());
-}
+export const printId = (id: NodeId) => {
+  console.log(id.toString());
+};
 
-export const printNodeId = (node: Node) => {	
-	console.log(node.id.toString());
-}
+export const printNodeId = (node: Node) => {
+  console.log(node.id.toString());
+};
 
-export const printIds = (ids: ID[]) => {
-	console.log(ids.map(id => id.toString()));
-}
+export const printIds = (ids: NodeId[]) => {
+  console.log(ids.map((id) => id.toString()));
+};
 
 export const printNodeIds = (nodes: Node[]) => {
-	console.log(nodes.map(n => n.id.toString()));
-}
+  console.log(nodes.map((n) => n.id.toString()));
+};
 
 export const printNodeNames = (nodes: Node[]) => {
-	console.log(nodes.map(n => n.name));
-}
+  console.log(nodes.map((n) => n.name));
+};
